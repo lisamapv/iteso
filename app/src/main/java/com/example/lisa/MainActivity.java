c9bb11d61033eda0aa55708ea401b80ace3b63f6
Cursos LABDESI <Cursos@T20213.ds.iteso.mx>
2019-07-09 14:39:17 -0500

    partel app

---
+++ app/src/main/java/com/example/lisa/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.lisa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.main_start_btn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.bounce);
+                startBtn.startAnimation(animation);
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+            MainActivity.this.startActivity(intent);
+            }
+        });
+
+    }
+}
